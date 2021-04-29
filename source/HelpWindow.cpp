7b3c7502350732e8a3349c9943a64a8884f3205e
Benjamin Letnes Bjerken <benjabj@stud.ntnu.no>
2021-04-29 15:42:34 +0200

    Added help window content

--- source/HelpWindow.cpp
+++ source/HelpWindow.cpp
@@ -1,9 +1,14 @@
 #include "HelpWindow.h"
 
 namespace ftxui {
-	// TODO: Implement help window content
+	// https://en.wikipedia.org/wiki/Arrow_(symbol)
 	Element HelpWindow::Render() {
-		Element ele = window(text(L"Help menu"), text(L""));
-		return ele;
+		return window(text(L"Help menu"),
+		vbox({
+				text(L"Use the enter/space key to click on options."),
+				// left up down right
+				hbox({text(L"Navigate with the arrow keys "), text(L"\x2190 \x2191 \x2193 \x2192") | bold}),
+				text(L"Sort the tasks with the 'F' keys (for example: F1)")
+			}));
 	}
 }
